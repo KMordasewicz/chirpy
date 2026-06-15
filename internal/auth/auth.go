e48e409a244e812498021b81bfbcccef0fb4418d
KMordasewicz <krzysztof.m.mordasewicz@gmail.com>
2026-06-15 22:53:54 +0200

    Add JWT to auth

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,8 +1,18 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
+	"time"
+
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
+
+type TokenType string
+
+const TokenTypeAccess TokenType = "chirpy-access"
 
 func HashPassword(password string) (string, error) {
 	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -18,4 +28,55 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return false, err
 	}
 	return match, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		&jwt.RegisteredClaims{
+			Issuer:    string(TokenTypeAccess),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+			Subject:   userID.String(),
+		},
+	)
+
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claimsStruct := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&claimsStruct,
+		func(token *jwt.Token) (any, error) {
+			return []byte(tokenSecret), nil
+		},
+	)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if !token.Valid {
+		return uuid.Nil, errors.New("token is invalid")
+	}
+
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != string(TokenTypeAccess) {
+		return uuid.Nil, fmt.Errorf("incorrect issuer: %v", issuer)
+	}
+
+	subject, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	id, err := uuid.Parse(subject)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return id, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,7 +1,12 @@
 package auth
 
 import (
+	"fmt"
 	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func TestHashPassword(t *testing.T) {
@@ -33,5 +38,122 @@ func TestHashPassword(t *testing.T) {
 		if c.expectedMatch != match {
 			t.Errorf("Incorrect password validation for password: %v and input %v", c.password, c.inputPassword)
 		}
+	}
+}
+
+func TestMakeJWT(t *testing.T) {
+	longDuration, _ := time.ParseDuration("30m")
+	uuid1, _ := uuid.Parse("402d8009-dec5-4e32-b585-6db9e77776c3")
+	uuid2, _ := uuid.Parse("8519a403-42fc-4923-8719-e8766e60c80c")
+	testCases := []struct {
+		name         string
+		userID       uuid.UUID
+		tokenSecret  string
+		exipiersIn   time.Duration
+		expectedFail bool
+	}{
+		{
+			name:         "Good path",
+			userID:       uuid1,
+			tokenSecret:  "pa$$word",
+			exipiersIn:   longDuration,
+			expectedFail: false,
+		},
+		{
+			name:         "Good path 2",
+			userID:       uuid2,
+			tokenSecret:  "😀", // Why does it work!?
+			exipiersIn:   longDuration,
+			expectedFail: false,
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			_, err := MakeJWT(c.userID, c.tokenSecret, c.exipiersIn)
+			if (err != nil) != c.expectedFail {
+				fmt.Printf("MakeJWT() error = %v, expectedFail = %v\n", err, c.expectedFail)
+				return
+			}
+		})
+	}
+}
+
+func TestValidateJWT(t *testing.T) {
+	longDuration, _ := time.ParseDuration("30m")
+	shortDuration, _ := time.ParseDuration("1s")
+	uuid1, _ := uuid.Parse("402d8009-dec5-4e32-b585-6db9e77776c3")
+	uuid2, _ := uuid.Parse("8519a403-42fc-4923-8719-e8766e60c80c")
+	testCases := []struct {
+		name              string
+		userID            uuid.UUID
+		tokenSecret       string
+		exipiersIn        time.Duration
+		passCorrectSecret bool
+		expectedFail      bool
+	}{
+		{
+			name:              "Good path",
+			userID:            uuid1,
+			tokenSecret:       "pa$$word",
+			exipiersIn:        longDuration,
+			passCorrectSecret: true,
+			expectedFail:      false,
+		},
+		{
+			name:              "Wrong password",
+			userID:            uuid2,
+			tokenSecret:       "test123",
+			exipiersIn:        longDuration,
+			passCorrectSecret: false,
+			expectedFail:      true,
+		},
+		{
+			name:              "Expired token",
+			userID:            uuid1,
+			tokenSecret:       "ha$hword",
+			exipiersIn:        shortDuration,
+			passCorrectSecret: true,
+			expectedFail:      true,
+		},
+		{
+			name:              "Wrong password and expired token",
+			userID:            uuid2,
+			tokenSecret:       "321test",
+			exipiersIn:        shortDuration,
+			passCorrectSecret: false,
+			expectedFail:      true,
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			synctest.Test(t, func(t *testing.T) {
+				token, err := MakeJWT(c.userID, c.tokenSecret, c.exipiersIn)
+				if err != nil {
+					t.Errorf("Token creation failed:\n\tuserID: %v\n\ttokenSecret: %v\n\texpiresIn: %v\nWith error:\n%v", c.userID, c.tokenSecret, c.exipiersIn, err)
+					return
+				}
+
+				var secret string
+				if c.passCorrectSecret {
+					secret = c.tokenSecret
+				} else {
+					secret = "incorrectSecret123$"
+				}
+
+				time.Sleep(3 * time.Second)
+
+				uuid, err := ValidateJWT(token, secret)
+				if (err != nil) != c.expectedFail {
+					fmt.Printf("ValidateJWT() error = %v, expectedFail = %v\n", err, c.expectedFail)
+					return
+				}
+
+				if !c.expectedFail && uuid != c.userID {
+					t.Errorf("uuid mismatch, got: %v instead: %v\n", uuid, c.userID)
+				}
+			})
+		})
 	}
 }
